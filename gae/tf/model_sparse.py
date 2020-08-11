337f897dcc5439346aa7cdacca34c04cd52881b3
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-08-11 09:39:29 +0100

    Retain sparse implementations.

---
+++ gae/tf/model_sparse.py
@@ -0,0 +1,166 @@
+'''
+(c) University of Liverpool 2020
+
+All rights reserved.
+
+@author: neilswainston
+'''
+# pylint: disable=invalid-name
+# pylint: disable=no-member
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=wrong-import-order
+from gae.tf.layers import GraphConvolution, GraphConvolutionSparse, \
+    InnerProductDecoder
+import tensorflow as tf
+
+
+def get_model(placeholders, num_features, num_nonzero_feats,
+              hidden1, hidden2, num_nodes, is_ae):
+    '''Get model.'''
+    if is_ae:
+        return GCNModelAE(placeholders['features'],
+                          placeholders['adj'],
+                          placeholders['dropout'],
+                          num_features,
+                          num_nonzero_feats,
+                          hidden1=hidden1,
+                          hidden2=hidden2)
+    # else:
+    return GCNModelVAE(placeholders['features'],
+                       placeholders['adj'],
+                       placeholders['dropout'],
+                       num_features,
+                       num_nodes,
+                       num_nonzero_feats,
+                       hidden1=hidden1, hidden2=hidden2)
+
+
+class Model():
+    '''Model baseclass.'''
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name', self.__class__.__name__.lower())
+        self.logging = kwargs.get('logging', False)
+        self.vars = {}
+
+        self.z_mean = None
+        self.reconstructions = None
+
+    def _build(self):
+        raise NotImplementedError
+
+    def build(self):
+        ''' Wrapper for _build() '''
+        with tf.compat.v1.variable_scope(self.name):
+            self._build()
+
+        variables = tf.compat.v1.get_collection(
+            tf.compat.v1.GraphKeys.GLOBAL_VARIABLES, scope=self.name)
+
+        self.vars = {var.name: var for var in variables}
+
+    def fit(self):
+        '''Fit.'''
+
+    def predict(self):
+        '''Predict.'''
+
+
+class GCNModelAE(Model):
+    '''GCN model autoencoder.'''
+
+    def __init__(self, inputs, adj, dropout, num_features, num_nonzero_feats,
+                 hidden1, hidden2, **kwargs):
+        super(GCNModelAE, self).__init__(**kwargs)
+
+        self.inputs = inputs
+        self.input_dim = num_features
+        self.num_nonzero_feats = num_nonzero_feats
+        self.adj = adj
+        self.dropout = dropout
+        self.hidden1 = hidden1
+        self.hidden2 = hidden2
+        self.build()
+
+    def _build(self):
+        hidden_layer1 = GraphConvolutionSparse(
+            input_dim=self.input_dim,
+            output_dim=self.hidden1,
+            adj=self.adj,
+            num_nonzero_feats=self.num_nonzero_feats,
+            act=tf.nn.relu,
+            dropout=self.dropout,
+            logging=self.logging)(self.inputs)
+
+        self.z_mean = GraphConvolution(
+            input_dim=self.hidden1,
+            output_dim=self.hidden2,
+            adj=self.adj,
+            act=lambda x: x,
+            dropout=self.dropout,
+            logging=self.logging)(hidden_layer1)
+
+        self.reconstructions = InnerProductDecoder(
+            act=lambda x: x,
+            dropout=0.0,
+            logging=self.logging)(self.z_mean)
+
+
+class GCNModelVAE(Model):
+    '''GCN model variational autoencoder.'''
+
+    def __init__(self, inputs, adj, dropout, num_features, num_nodes,
+                 num_nonzero_feats, hidden1, hidden2, **kwargs):
+        super(GCNModelVAE, self).__init__(**kwargs)
+
+        self.inputs = inputs
+        self.input_dim = num_features
+        self.num_nonzero_feats = num_nonzero_feats
+        self.n_samples = num_nodes
+        self.adj = adj
+        self.dropout = dropout
+        self.hidden1 = hidden1
+        self.hidden2 = hidden2
+
+        self.hidden_layer1 = None
+        self.z = None
+        self.z_mean = None
+        self.z_log_std = None
+
+        self.build()
+
+    def _build(self):
+        hidden_layer1 = GraphConvolutionSparse(
+            input_dim=self.input_dim,
+            output_dim=self.hidden1,
+            adj=self.adj,
+            num_nonzero_feats=self.num_nonzero_feats,
+            act=tf.nn.relu,
+            dropout=self.dropout,
+            logging=self.logging)(self.inputs)
+
+        self.z_mean = GraphConvolution(
+            input_dim=self.hidden1,
+            output_dim=self.hidden2,
+            adj=self.adj,
+            act=lambda x: x,
+            dropout=self.dropout,
+            logging=self.logging)(hidden_layer1)
+
+        self.z_log_std = GraphConvolution(
+            input_dim=self.hidden1,
+            output_dim=self.hidden2,
+            adj=self.adj,
+            act=lambda x: x,
+            dropout=self.dropout,
+            logging=self.logging)(hidden_layer1)
+
+        z = self.z_mean + \
+            tf.random.normal(
+                [self.n_samples, self.hidden2]) * tf.exp(self.z_log_std)
+
+        self.reconstructions = InnerProductDecoder(
+            act=lambda x: x,
+            dropout=0.0,
+            logging=self.logging)(z)
---
+++ gae/tf/train_single_sparse.py
@@ -0,0 +1,123 @@
+'''
+(c) University of Liverpool 2020
+
+All rights reserved.
+
+@author: neilswainston
+'''
+# pylint: disable=invalid-name
+# pylint: disable=no-name-in-module
+# pylint: disable=too-many-arguments
+# pylint: disable=too-many-locals
+# pylint: disable=wrong-import-order
+import os
+import time
+
+from scipy.special import expit
+from sklearn.metrics import average_precision_score, roc_auc_score
+
+from gae.data import load_data
+from gae.preprocessing_single import preprocess_adj, preprocess_feat, \
+    sparse_to_tuple
+from gae.tf.model import get_model
+from gae.tf.optimizer import get_opt
+import numpy as np
+import scipy.sparse as sp
+import tensorflow as tf
+
+
+# Train on CPU (hide GPU) due to memory constraints
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+
+
+def train(adj, features, is_ae=True, use_features=True,
+          epochs=200, dropout=0.0, hidden1=256, hidden2=128,
+          learning_rate=0.01):
+    '''train.'''
+
+    # Adjacency:
+    adj_norm = preprocess_adj(adj)
+
+    # Features:
+    features, num_features, num_nonzero_feats = \
+        preprocess_feat(features, use_features)
+
+    # Define placeholders:
+    placeholders = {
+        'features': tf.compat.v1.sparse_placeholder(tf.float32),
+        'adj': tf.compat.v1.sparse_placeholder(tf.float32),
+        'adj_orig': tf.compat.v1.sparse_placeholder(tf.float32),
+        'dropout': tf.compat.v1.placeholder_with_default(0., shape=())
+    }
+
+    # Create model:
+    model = get_model(placeholders, num_features, num_nonzero_feats,
+                      hidden1, hidden2, adj.shape[0], is_ae)
+
+    # Optimizer:
+    opt = get_opt(model, adj, placeholders['adj_orig'], learning_rate, is_ae)
+
+    # Initialize session:
+    sess = tf.compat.v1.Session()
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    # Construct feed dictionary:
+    feed_dict = {
+        placeholders['features']: features,
+        placeholders['adj']: adj_norm,
+        placeholders['adj_orig']: sparse_to_tuple(adj + sp.eye(adj.shape[0])),
+        placeholders['dropout']: dropout
+    }
+
+    # Train model:
+    for epoch in range(epochs):
+        t = time.time()
+
+        # Run single weight update:
+        _, avg_cost, avg_accuracy = sess.run(
+            [opt.opt_op, opt.cost, opt.accuracy],
+            feed_dict=feed_dict)
+
+        # roc_curr, ap_curr = get_roc_score(
+        #    adj_orig, val_edges, val_edges_false, adj_rec)
+
+        print('Epoch:', '%05d' % (epoch + 1),
+              'train_loss=', '{:.5f}'.format(avg_cost),
+              'train_acc=', '{:.5f}'.format(avg_accuracy),
+              # 'val_roc=', '{:.5f}'.format(roc_curr),
+              # 'val_ap=', '{:.5f}'.format(ap_curr),
+              'time=', '{:.5f}'.format(time.time() - t))
+
+    adj_rec = _get_adj_rec(sess, model, feed_dict)
+    roc_score, ap_score = _get_roc_score(adj, adj_rec)
+
+    print('Test ROC score: ' + str(roc_score))
+    print('Test AP score: ' + str(ap_score))
+
+
+def _get_adj_rec(sess, model, feed_dict):
+    '''Get reconstructed adjacency matrix.'''
+    emb = sess.run(model.z_mean, feed_dict=feed_dict)
+    adj_rec = expit(np.dot(emb, emb.T))
+    return (adj_rec + 0.5).astype(np.int)
+
+
+def _get_roc_score(adj, adj_rec):
+    '''Get ROC score.'''
+    adj = adj.toarray().flatten()
+    adj_rec = adj_rec.flatten()
+    return roc_auc_score(adj, adj_rec), average_precision_score(adj, adj)
+
+
+def main():
+    '''main method.'''
+
+    # Load data:
+    adj, features = load_data('cora')
+
+    # Train:
+    train(adj, features, is_ae=False)
+
+
+if __name__ == '__main__':
+    main()
